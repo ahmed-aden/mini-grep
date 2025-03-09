7f0bf4390370e317b5fc978f1341463f9dfa82a5
Ahmed <ahmed.aden14@icloud.com>
2025-03-10 00:38:05 +1100

    add config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_configs(&args);
+
+    println!("looking for query: {}", config.query);
+    println!("looking for file_path: {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+
+}
+
+fn parse_configs(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
 }
